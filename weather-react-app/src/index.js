5ae62ee4dc3f7ac9790017305e211e4680b466c4
StardustIsArt <yocius_sa@icloud.com>
2022-02-10 11:56:20 -0600

    making corrections with correct files

--- weather-react-app/src/index.js
+++ weather-react-app/src/index.js
@@ -1,22 +1,10 @@
-import React from 'react';
-import { StrictMode } from "react";
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-import Data from "./Data";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.Strict>
-    <App />
-  </React.Strict>,
+  <React.Strict></React.Strict>,
   rootElement,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
